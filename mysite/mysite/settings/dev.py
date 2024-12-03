aacf4e25117e96af58ef59dd82f9be4ade896a5a
ivanruiperez <ivanruiperez.instituto@gmail.com>
2024-12-03 12:02:31 +0000

    desarrollo check

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b5qpxbqqj$%rao(_qpj=-3*p55i^f45q_a78i)%c&k2*u_9n+d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
